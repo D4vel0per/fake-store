4b0b50eb23120c861b184b78455d80d041a859f7
D4veloper <omardavidmendezmontiel@gmail.com>
2026-08-23 17:45:00 -0400

    initial commit with routes already thought out

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login", // Auth Layout
+        children: [
+            {
+                path: '', // Auth Component
+            },
+            {
+                path: '**' // Auth Component
+            }
+        ],
+    },
+    {
+        path: "dashboard", // Dashboard layout
+        children: [
+            {
+                path: '' // Dashboard Page
+            },
+            {
+                path: 'carts', // Carts Page
+                children: [ 
+                    { path: 'add' },
+                    { path: 'edit/:cartId' }
+                ]
+            },
+            {
+                path: 'products', // Products Page
+                children: [
+                    { path: 'add' },
+                    { path: 'edit/:productId' }
+                ]
+            }
+        ]
+    },
+    { path: "", redirectTo: "dashboard" },
+    { path: "**", redirectTo: "dashboard" }
+
+];
